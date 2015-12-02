24c892eb66ccd03d606ef9d43ba90bc9e890936d
Travis Brown <travisrobertbrown@gmail.com>
2015-12-02 16:06:57 -0500

    Update README and benchmarks

--- README.md
+++ README.md
@@ -8,6 +8,28 @@
 This is a quick port of [Scalaz][scalaz]'s [iteratee implementation][scalaz-iteratee] to
 [cats][cats].
 
+The motivations for the port are similar to those for [circe][circe]—in particular I'm aiming for a
+more consistent API, better performance, and better documentation.
+
+So far I've made two major optimizations: I've added chunking for input and `InputFolder` and
+`StepFolder` classes that combine "functions" for folds into a single object. The initial results
+look promising:
+
+```
+Benchmark                      Mode  Cnt     Score    Error  Units
+IterateeBenchmark.sumIntsI    thrpt   20  2161.012 ± 76.171  ops/s
+IterateeBenchmark.sumIntsS    thrpt   20   529.520 ± 15.892  ops/s
+IterateeBenchmark.takeLongsI  thrpt   20   953.304 ±  7.715  ops/s
+IterateeBenchmark.takeLongsS  thrpt   20   369.583 ±  2.474  ops/s
+```
+
+```
+IterateeBenchmark.sumIntsI:gc.alloc.rate.norm   thrpt   20   1760856.770 ±       1.491    B/op
+IterateeBenchmark.sumIntsS:gc.alloc.rate.norm   thrpt   20   8001675.187 ±      34.398    B/op
+IterateeBenchmark.takeLongsI:gc.alloc.rate.norm thrpt   20   4724417.873 ±       3.656    B/op
+IterateeBenchmark.takeLongsS:gc.alloc.rate.norm thrpt   20  11685721.549 ±  356398.849    B/op
+```
+
 ## License
 
 iteratee is licensed under the **[Apache License, Version 2.0][apache]** (the
@@ -21,5 +43,6 @@ limitations under the License.
 
 [apache]: http://www.apache.org/licenses/LICENSE-2.0
 [cats]: https://github.com/non/cats
+[circe]: https://github.com/travisbrown/circe
 [scalaz]: https://github.com/scalaz/scalaz
 [scalaz-iteratee]: https://github.com/scalaz/scalaz/tree/series/7.2.x/iteratee/src/main/scala/scalaz/iteratee
--- benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
+++ benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
@@ -1,5 +1,6 @@
 package io.iteratee.benchmark
 
+import cats.std.int._
 import cats.std.list.{ listAlgebra, listInstance => listInstanceC }
 import io.{ iteratee => i }
 import java.util.concurrent.TimeUnit
@@ -12,10 +13,12 @@ import scalaz.std.vector._
 class ExampleData {
   val maxSize = 10000
   val intsI: i.Enumerator[Int, cats.Eval] = i.Enumerator.enumList((0 to maxSize).toList)
-  val intsS: s.EnumeratorT[Int, scalaz.Free.Trampoline] = s.EnumeratorT.enumList((0 to maxSize).toList)
+  val intsS: s.EnumeratorT[Int, scalaz.Free.Trampoline] =
+    s.EnumeratorT.enumList((0 to maxSize).toList)
 
   val longsI: i.Enumerator[Long, cats.Eval] = i.Enumerator.iterate[Long, cats.Eval](_ + 1L, 0L)
-  val longsS: s.EnumeratorT[Long, scalaz.Free.Trampoline] = s.EnumeratorT.iterate[Long, scalaz.Free.Trampoline](_ + 1L, 0L)
+  val longsS: s.EnumeratorT[Long, scalaz.Free.Trampoline] =
+    s.EnumeratorT.iterate[Long, scalaz.Free.Trampoline](_ + 1L, 0L)
 }
 
 /**
@@ -23,21 +26,22 @@ class ExampleData {
  *
  * The following command will run the benchmarks with reasonable settings:
  *
- * > sbt "benchmark/run -i 10 -wi 10 -f 2 -t 1 io.travisbrown.benchmark.IterateeBenchmark"
+ * > sbt "benchmark/run -i 10 -wi 10 -f 2 -t 1 io.iteratee.benchmark.IterateeBenchmark"
  */
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
 class IterateeBenchmark extends ExampleData {
-  //@Benchmark
-  //def sumIntsI: Int = i.IterateeT.sum[Int, cats.Eval].feedE(intsI).run.value
+  @Benchmark
+  def sumIntsI: Int = i.Iteratee.sum[Int, cats.Eval].feedE(intsI).run.value
 
-  //@Benchmark
-  //def sumIntsS: Int = (s.IterateeT.sum[Int, scalaz.Free.Trampoline] &= intsS).run.run
+  @Benchmark
+  def sumIntsS: Int = (s.IterateeT.sum[Int, scalaz.Free.Trampoline] &= intsS).run.run
 
   @Benchmark
   def takeLongsI: Vector[Long] = i.Iteratee.take[Long, cats.Eval](10000).feedE(longsI).run.value
 
   @Benchmark
-  def takeLongsS: Vector[Long] = (s.Iteratee.take[Long, Vector](10000).up[scalaz.Free.Trampoline] &= longsS).run.run
+  def takeLongsS: Vector[Long] =
+    (s.Iteratee.take[Long, Vector](10000).up[scalaz.Free.Trampoline] &= longsS).run.run
 }
