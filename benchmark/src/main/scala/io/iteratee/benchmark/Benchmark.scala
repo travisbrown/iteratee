3860507e21cc8e6cc6ddce1c584a6f291a8c434b
Travis Brown <travisrobertbrown@gmail.com>
2015-12-04 11:41:35 -0500

    Add standard library and scalaz-stream to benchmarks

--- benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
+++ benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
@@ -1,24 +1,30 @@
 package io.iteratee.benchmark
 
+import cats.Eval
 import cats.std.int._
 import cats.std.list.{ listAlgebra, listInstance => listInstanceC }
 import io.{ iteratee => i }
 import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations._
-import scalaz.{ iteratee => s }
+import scalaz.Free.Trampoline
+import scalaz.concurrent.Task
+import scalaz.{ iteratee => z }
 import scalaz.std.anyVal.intInstance
 import scalaz.std.list.{ listInstance => listInstanceS, listMonoid }
 import scalaz.std.vector._
+import scalaz.stream.Process
 
 class ExampleData {
   val maxSize = 10000
-  val intsI: i.Enumerator[Int, cats.Eval] = i.Enumerator.enumVector((0 to maxSize).toVector)
-  val intsS: s.EnumeratorT[Int, scalaz.Free.Trampoline] =
-    s.EnumeratorT.enumList((0 to maxSize).toList)
+  val intsC: Stream[Int] = Stream.from(0).take(maxSize)
+  val intsI: i.Enumerator[Int, Eval] = i.Enumerator.enumStream(Stream.from(0).take(maxSize))
+  val intsZ: z.EnumeratorT[Int, Trampoline] = z.EnumeratorT.enumStream(Stream.from(0).take(maxSize))
+  val intsS: Process[Task, Int] = Process.range(0, maxSize)
 
-  val longsI: i.Enumerator[Long, cats.Eval] = i.Enumerator.iterate[Long, cats.Eval](0L)(_ + 1L)
-  val longsS: s.EnumeratorT[Long, scalaz.Free.Trampoline] =
-    s.EnumeratorT.iterate[Long, scalaz.Free.Trampoline](_ + 1L, 0L)
+  val longsC: Stream[Long] = Stream.iterate(0L)(_ + 1L)
+  val longsI: i.Enumerator[Long, Eval] = i.Enumerator.iterate[Long, Eval](0L)(_ + 1L)
+  val longsZ: z.EnumeratorT[Long, Trampoline] = z.EnumeratorT.iterate[Long, Trampoline](_ + 1L, 0L)
+  val longsS: Process[Task, Long] = Process.iterate(0L)(_ + 1L)
 }
 
 /**
@@ -33,15 +39,27 @@ class ExampleData {
 @OutputTimeUnit(TimeUnit.SECONDS)
 class IterateeBenchmark extends ExampleData {
   @Benchmark
-  def sumIntsI: Int = i.Iteratee.sum[Int, cats.Eval].process(intsI).value
+  def sumIntsI: Int = i.Iteratee.sum[Int, Eval].process(intsI).value
 
   @Benchmark
-  def sumIntsS: Int = (s.IterateeT.sum[Int, scalaz.Free.Trampoline] &= intsS).run.run
+  def sumIntsC: Int = intsC.sum
 
   @Benchmark
-  def takeLongsI: Vector[Long] = i.Iteratee.take[Long, cats.Eval](10000).process(longsI).value
+  def sumIntsZ: Int = (z.IterateeT.sum[Int, Trampoline] &= intsZ).run.run
 
   @Benchmark
-  def takeLongsS: Vector[Long] =
-    (s.Iteratee.take[Long, Vector](10000).up[scalaz.Free.Trampoline] &= longsS).run.run
+  def sumIntsS: Int = intsS.sum.runLastOr(sys.error("Impossible")).run
+
+  @Benchmark
+  def takeLongsI: Vector[Long] = i.Iteratee.take[Long, Eval](10000).process(longsI).value
+
+  @Benchmark
+  def takeLongsC: Vector[Long] = longsC.take(maxSize).toVector
+
+  @Benchmark
+  def takeLongsZ: Vector[Long] =
+    (z.Iteratee.take[Long, Vector](maxSize).up[Trampoline] &= longsZ).run.run
+
+  @Benchmark
+  def takeLongsS: Vector[Long] = longsS.take(maxSize).runLog.run
 }
