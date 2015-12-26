20970815b184522cc8594d13a375db52201d5e91
Travis Brown <travisrobertbrown@gmail.com>
2015-12-26 09:30:19 -0700

    Run all benchmarks in Task

--- benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
+++ benchmark/src/main/scala/io/iteratee/benchmark/Benchmark.scala
@@ -1,14 +1,12 @@
 package io.iteratee.benchmark
 
 import cats.Eval
-import cats.free.{ Trampoline => cTrampoline }
-import cats.std.function._
 import cats.std.int._
 import cats.std.list.{ listAlgebra, listInstance => listInstanceC }
 import io.{ iteratee => i }
+import io.iteratee.task._
 import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations._
-import scalaz.Free.{ Trampoline => zTrampoline }
 import scalaz.concurrent.Task
 import scalaz.{ iteratee => z }
 import scalaz.std.anyVal.intInstance
@@ -19,18 +17,16 @@ import scalaz.stream.Process
 class InMemoryExampleData {
   val size = 10000
   val intsC: Vector[Int] = (0 until size).toVector
-  val intsI: i.Enumerator[cTrampoline, Int] = i.Enumerator.enumVector(intsC)
+  val intsI: i.Enumerator[Task, Int] = i.Enumerator.enumVector(intsC)
   val intsS: Process[Task, Int] = Process.emitAll(intsC)
-  val intsZ: z.EnumeratorT[Int, zTrampoline] = z.EnumeratorT.enumIndexedSeq(intsC)
+  val intsZ: z.EnumeratorT[Int, Task] = z.EnumeratorT.enumIndexedSeq(intsC)
 }
 
 class StreamingExampleData {
   val longStreamC: Stream[Long] = Stream.iterate(0L)(_ + 1L)
-  val longStreamI: i.Enumerator[cTrampoline, Long] =
-    i.Enumerator.iterate[cTrampoline, Long](0L)(_ + 1L)
+  val longStreamI: i.Enumerator[Task, Long] = i.Enumerator.iterate[Task, Long](0L)(_ + 1L)
   val longStreamS: Process[Task, Long] = Process.iterate(0L)(_ + 1L)
-  val longStreamZ: z.EnumeratorT[Long, zTrampoline] =
-    z.EnumeratorT.iterate[Long, zTrampoline](_ + 1L, 0L)
+  val longStreamZ: z.EnumeratorT[Long, Task] = z.EnumeratorT.iterate[Long, Task](_ + 1L, 0L)
 }
 
 /**
@@ -48,13 +44,13 @@ class InMemoryBenchmark extends InMemoryExampleData {
   def sumIntsC: Int = intsC.sum
 
   @Benchmark
-  def sumIntsI: Int = intsI.run(i.Iteratee.sum[cTrampoline, Int]).run
+  def sumIntsI: Int = intsI.run(i.Iteratee.sum[Task, Int]).run
 
   @Benchmark
   def sumIntsS: Int = intsS.sum.runLastOr(sys.error("Impossible")).run
 
   @Benchmark
-  def sumIntsZ: Int = (z.IterateeT.sum[Int, zTrampoline] &= intsZ).run.run
+  def sumIntsZ: Int = (z.IterateeT.sum[Int, Task] &= intsZ).run.run
 }
 
 /**
@@ -74,12 +70,12 @@ class StreamingBenchmark extends StreamingExampleData {
   def takeLongsC: Vector[Long] = longStreamC.take(size).toVector
 
   @Benchmark
-  def takeLongsI: Vector[Long] = longStreamI.run(i.Iteratee.take[cTrampoline, Long](size)).run
+  def takeLongsI: Vector[Long] = longStreamI.run(i.Iteratee.take[Task, Long](size)).run
 
   @Benchmark
   def takeLongsS: Vector[Long] = longStreamS.take(size).runLog.run
 
   @Benchmark
   def takeLongsZ: Vector[Long] =
-    (z.Iteratee.take[Long, Vector](size).up[zTrampoline] &= longStreamZ).run.run
+    (z.Iteratee.take[Long, Vector](size).up[Task] &= longStreamZ).run.run
 }
