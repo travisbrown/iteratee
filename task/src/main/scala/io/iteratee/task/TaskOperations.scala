052b4bafb6a5c69b07b460274377d5a8bd1723b1
Travis Brown <travisrobertbrown@gmail.com>
2015-12-30 11:41:29 -0600

    Rearrange task module

---
+++ task/src/main/scala/io/iteratee/task/TaskOperations.scala
@@ -0,0 +1,33 @@
+package io.iteratee.task
+
+import io.iteratee.Enumerator
+import io.iteratee.internal.{ Input, MapContStepFolder, Step }
+import java.io.{ BufferedReader, File, FileReader }
+import scalaz.concurrent.Task
+
+/**
+ * @groupname Task Task operations and instances
+ * @groupprio Task -1
+ */
+trait TaskOperations {
+  /**
+   * @group Task
+   */
+  final def lines(path: String): Enumerator[Task, String] =
+    Enumerator.liftM(Task(new BufferedReader(new FileReader(new File(path))))).flatMap { reader =>
+      new LineEnumerator(reader).ensure(Task(reader.close()))
+    }
+
+  private[this] final class LineEnumerator(reader: BufferedReader) extends Enumerator[Task, String] {
+    final def apply[A](s: Step[Task, String, A]): Task[Step[Task, String, A]] = s.foldWith(
+      new MapContStepFolder[Task, String, A](s) {
+        def onCont(k: Input[String] => Task[Step[Task, String, A]]): Task[Step[Task, String, A]] = {
+          Task(reader.readLine()).flatMap {
+            case null => Task.taskInstance.point(s)
+            case line => k(Input.el(line)).flatMap(apply)
+          }
+        }
+      }
+    )
+  }
+}
