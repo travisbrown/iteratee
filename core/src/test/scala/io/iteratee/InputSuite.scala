b9700391d60e44718ee5579a545e7a4260032268
Travis Brown <travisrobertbrown@gmail.com>
2016-01-01 15:18:10 -0600

    Miscellaneous clean-up

--- core/src/test/scala/io/iteratee/InputSuite.scala
+++ core/src/test/scala/io/iteratee/InputSuite.scala
@@ -1,26 +1,18 @@
 package io.iteratee
 
-import algebra.{ Eq, Semigroup }
-import algebra.laws.GroupLaws
 import cats.Show
 import cats.std.{ IntInstances, OptionInstances }
-import cats.laws.discipline.{ MonadTests, SemigroupKTests, TraverseTests }
 import io.iteratee.internal.Input
 import org.scalatest.FunSuite
 import org.typelevel.discipline.scalatest.Discipline
 
 class InputSuite extends FunSuite with Discipline with IntInstances with OptionInstances
   with ArbitraryInstances {
-  /*checkAll("Input[Int]", GroupLaws[Input[Int]].semigroup)
-  checkAll("Input[Int]", MonadTests[Input].monad[Int, Int, Int])
-  checkAll("Input[Int]", TraverseTests[Input].traverse[Int, Int, Int, Int, Option, Option])
-
   test("Show[Input[Int]]") {
     def show(in: Input[Int]): String = Show[Input[Int]].show(in)
 
-    show(Input.empty[Int]) === "empty" &&
     show(Input.el(0)) === "el(0)" &&
     show(Input.chunk(Vector(0, 1))) === "chunk(0, 1)" &&
     show(Input.end[Int]) === "end"
-  }*/
+  }
 }
