0e5f356d37ddd63dd6ab6431c30f57f172693557
Travis Brown <travisrobertbrown@gmail.com>
2016-01-03 13:41:36 -0600

    Working but slow

--- core/src/main/scala/io/iteratee/internal/Input.scala
+++ core/src/main/scala/io/iteratee/internal/Input.scala
@@ -16,7 +16,7 @@ sealed abstract class Input[@specialized E] extends Serializable {
    * Reduce this [[Input]] to a value using the given pair of functions.
    */
   final def fold[Z](end: => Z, values: Vector[E] => Z): Z = foldWith(
-    new Input.Folder[E, Z] {
+    new Input.FolderX[E, Z] {
       //final def onEndX: Z = end
       final def onEl(e: E): Z = values(Vector(e))
       final def onChunk(h1: E, h2: E, es: Vector[E]): Z = values(h1 +: h2 +: es)
@@ -30,7 +30,7 @@ sealed abstract class Input[@specialized E] extends Serializable {
    * expense of allocating multiple function objects and collection instances is
    * known to be too high. In most cases [[fold]] should be preferred.
    */
-  def foldWith[Z](folder: Input.Folder[E, Z]): Z
+  def foldWith[Z](folder: Input.FolderX[E, Z]): Z
 
   def map[B](f: E => B): Input[B]
 
@@ -53,7 +53,7 @@ final object Input extends InputInstances {
    * @tparam E The element type
    * @tparam Z The result type
    */
-  trait Folder[@specialized E, Z] {
+  trait FolderX[@specialized E, Z] {
     //def onEndX: Z
     def onEl(e: E): Z
     def onChunk(h1: E, h2: E, es: Vector[E]): Z
@@ -63,7 +63,7 @@ final object Input extends InputInstances {
    * An input value containing a single element.
    */
   final def el[E](e: E): Input[E] = new Input[E] {
-    final def foldWith[Z](folder: Folder[E, Z]): Z = folder.onEl(e)
+    final def foldWith[Z](folder: FolderX[E, Z]): Z = folder.onEl(e)
     final def isEnd: Boolean = false
     final def map[B](f: E => B): Input[B] = el(f(e))
     private[iteratee] final def toVector: Vector[E] = Vector(e)
@@ -73,7 +73,7 @@ final object Input extends InputInstances {
    * An input value containing zero or more elements.
    */
   final def chunk[E](e1: E, e2: E, es: Vector[E]): Input[E] = new Input[E] {
-    final def foldWith[Z](folder: Folder[E, Z]): Z = folder.onChunk(e1, e2, es)
+    final def foldWith[Z](folder: FolderX[E, Z]): Z = folder.onChunk(e1, e2, es)
     final def isEnd: Boolean = false
     final def map[B](f: E => B): Input[B] = chunk(f(e1), f(e2), es.map(f))
     private[iteratee] final def toVector: Vector[E] = e1 +: e2 +: es
