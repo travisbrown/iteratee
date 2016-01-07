6310eaa8757fc45fe59217b64b5e89d6dc1d11bb
Travis Brown <travisrobertbrown@gmail.com>
2016-01-07 16:04:04 -0600

    Add mapK

--- core/src/main/scala/io/iteratee/EnumerateeModule.scala
+++ core/src/main/scala/io/iteratee/EnumerateeModule.scala
@@ -16,13 +16,19 @@ trait EnumerateeModule[F[_]] {
   final def map[O, I](f: O => I)(implicit F: Monad[F]): Enumeratee[F, O, I] = Enumeratee.map(f)
 
   /**
+   * Map a function returning a value in a context over a stream.
+   *
+   * @group Enumeratees
+   */
+  final def mapK[O, I](f: O => F[I])(implicit F: Monad[F]): Enumeratee[F, O, I] = Enumeratee.mapK(f)
+
+  /**
    * Map a function returning an [[Enumerator]] over a stream and flatten the
    * results.
    *
    * @group Enumeratees
    */
-  final def flatMap[O, I](f: O => Enumerator[F, I])(implicit F: Monad[F]): Enumeratee[F, O, I] =
-    Enumeratee.flatMap(f)
+  final def flatMap[O, I](f: O => Enumerator[F, I])(implicit F: Monad[F]): Enumeratee[F, O, I] = Enumeratee.flatMap(f)
 
   /**
    * Transform values using a [[scala.PartialFunction]] and drop values that
@@ -30,16 +36,14 @@ trait EnumerateeModule[F[_]] {
    *
    * @group Enumeratees
    */
-  final def collect[O, I](pf: PartialFunction[O, I])(implicit F: Monad[F]): Enumeratee[F, O, I] =
-    Enumeratee.collect(pf)
+  final def collect[O, I](pf: PartialFunction[O, I])(implicit F: Monad[F]): Enumeratee[F, O, I] = Enumeratee.collect(pf)
 
   /**
    * Drop values that do not satisfy the given predicate.
    *
    * @group Enumeratees
    */
-  final def filter[E](p: E => Boolean)(implicit F: Monad[F]): Enumeratee[F, E, E] =
-    Enumeratee.filter(p)
+  final def filter[E](p: E => Boolean)(implicit F: Monad[F]): Enumeratee[F, E, E] = Enumeratee.filter(p)
 
   /**
    * Apply the given [[Iteratee]] repeatedly.
@@ -62,24 +66,21 @@ trait EnumerateeModule[F[_]] {
    *
    * @group Enumeratees
    */
-  final def zipWithIndex[E](implicit F: Monad[F]): Enumeratee[F, E, (E, Long)] =
-    Enumeratee.zipWithIndex
+  final def zipWithIndex[E](implicit F: Monad[F]): Enumeratee[F, E, (E, Long)] = Enumeratee.zipWithIndex
 
   /**
    * Split the stream into groups of a given length.
    *
    * @group Enumeratees
    */
-  final def grouped[E](n: Int)(implicit F: Monad[F]): Enumeratee[F, E, Vector[E]] =
-    Enumeratee.grouped(n)
+  final def grouped[E](n: Int)(implicit F: Monad[F]): Enumeratee[F, E, Vector[E]] = Enumeratee.grouped(n)
 
   /**
    * Split the stream using the given predicate to identify delimiters.
    *
    * @group Enumeratees
    */
-  final def splitOn[E](p: E => Boolean)(implicit F: Monad[F]): Enumeratee[F, E, Vector[E]] =
-    Enumeratee.splitOn(p)
+  final def splitOn[E](p: E => Boolean)(implicit F: Monad[F]): Enumeratee[F, E, Vector[E]] = Enumeratee.splitOn(p)
 
   /**
    * Transform a stream by taking the cross-product with the given
@@ -88,5 +89,5 @@ trait EnumerateeModule[F[_]] {
    * @group Enumeratees
    */
   final def cross[E1, E2](e2: Enumerator[F, E2])(implicit F: Monad[F]): Enumeratee[F, E1, (E1, E2)] =
-    Enumeratee.cross(e2)
+   Enumeratee.cross(e2)
 }
