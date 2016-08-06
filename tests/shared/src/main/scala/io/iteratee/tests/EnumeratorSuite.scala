04e96e6edf886634c802f355e15207ee73f5f095
Travis Brown <travisrobertbrown@gmail.com>
2016-08-06 17:42:59 -0500

    Fix spelling now that Cats snapshot is fixed

--- tests/shared/src/main/scala/io/iteratee/tests/EnumeratorSuite.scala
+++ tests/shared/src/main/scala/io/iteratee/tests/EnumeratorSuite.scala
@@ -116,7 +116,7 @@ abstract class EnumeratorSuite[F[_]: Monad] extends ModuleSuite[F] {
     /**
      * Workaround for divergence during resolution on 2.10.
      */
-    val E: Eq[F[Option[F[Vector[String]]]]] = eqF(catsKernelStdEqForOptioin(eqF(Eq[Vector[String]])))
+    val E: Eq[F[Option[F[Vector[String]]]]] = eqF(catsKernelStdEqForOption(eqF(Eq[Vector[String]])))
 
     val enumeratorF: F[Option[Enumerator[F, String]]] = eav.enumerator.bindM(v => Option(enumOne(v.toString)))
 
