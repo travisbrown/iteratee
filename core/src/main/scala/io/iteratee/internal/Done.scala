8ec57075f4fde051770166ec8c018ff7c5564c39
Travis Brown <travisrobertbrown@gmail.com>
2016-08-28 12:23:50 -0500

    Simplify Done representation

--- core/src/main/scala/io/iteratee/internal/Done.scala
+++ core/src/main/scala/io/iteratee/internal/Done.scala
@@ -4,52 +4,34 @@ import cats.{ Applicative, Monad }
 import cats.data.NonEmptyVector
 import cats.arrow.FunctionK
 
-private[internal] abstract class BaseDone[F[_], E, A](implicit F: Applicative[F]) extends Step[F, E, A] {
-  def value: A
+private[internal] case class Done[F[_], E, A](
+  value: A,
+  remaining: Vector[E]
+)(implicit F: Applicative[F]) extends Step[F, E, A] {
   final def isDone: Boolean = true
   final def run: F[A] = F.pure(value)
   final def feedEl(e: E): F[Step[F, E, A]] = F.pure(this)
   final def feedChunk(h1: E, h2: E, t: Vector[E]): F[Step[F, E, A]] = F.pure(this)
-}
 
-private[internal] case class NoLeftovers[F[_]: Applicative, E, A](value: A) extends BaseDone[F, E, A] {
   final def fold[Z](ifCont: (NonEmptyVector[E] => F[Step[F, E, A]]) => Z, ifDone: (A, Vector[E]) => Z): Z =
-    ifDone(value, Vector.empty)
+    ifDone(value, remaining)
 
-  final def map[B](f: A => B): Step[F, E, B] = new NoLeftovers(f(value))
-  final def contramap[E2](f: E2 => E): Step[F, E2, A] = new NoLeftovers(value)
-  final def mapI[G[_]: Applicative](f: FunctionK[F, G]): Step[G, E, A] = new NoLeftovers(value)
-  final def bind[B](f: A => F[Step[F, E, B]])(implicit M: Monad[F]): F[Step[F, E, B]] = f(value)
-  final def zip[B](other: Step[F, E, B]): Step[F, E, (A, B)] = other.map((value, _))
-}
-
-private[internal] case class WithLeftovers[F[_]: Applicative, E, A](value: A, remaining: Input[E])
-  extends BaseDone[F, E, A] {
-  final def fold[Z](ifCont: (NonEmptyVector[E] => F[Step[F, E, A]]) => Z, ifDone: (A, Vector[E]) => Z): Z =
-    ifDone(value, remaining.toVector)
-
-  final def map[B](f: A => B): Step[F, E, B] = new WithLeftovers(f(value), remaining)
-  final def contramap[E2](f: E2 => E): Step[F, E2, A] = new NoLeftovers(value)
-  final def mapI[G[_]: Applicative](f: FunctionK[F, G]): Step[G, E, A] =
-    new WithLeftovers(value, remaining)
+  final def map[B](f: A => B): Step[F, E, B] = Done(f(value), remaining)
+  final def contramap[E2](f: E2 => E): Step[F, E2, A] = Done(value, Vector.empty)
+  final def mapI[G[_]: Applicative](f: FunctionK[F, G]): Step[G, E, A] = Done(value, remaining)
 
   final def bind[B](f: A => F[Step[F, E, B]])(implicit M: Monad[F]): F[Step[F, E, B]] =
     M.flatMap(f(value)) {
-      case NoLeftovers(otherValue) => M.pure(new WithLeftovers(otherValue, remaining))
-      case WithLeftovers(otherValue, otherRemaining) => M.pure(
-        new WithLeftovers(otherValue, otherRemaining.append(remaining))
-      )
-      case step => remaining.foldWith(
-        new Input.Folder[E, F[Step[F, E, B]]] {
-          def onEl(e: E): F[Step[F, E, B]] = step.feedEl(e)
-          def onChunk(h1: E, h2: E, t: Vector[E]): F[Step[F, E, B]] = step.feedChunk(h1, h2, t)
-        }
-      )
+      case Done(otherValue, otherRemaining) => F.pure(Done(otherValue, otherRemaining ++ remaining))
+      case step => remaining match {
+        case xs if xs.isEmpty => f(value)
+        case xs if xs.size == 1 => step.feedEl(xs.head)
+        case h1 +: h2 +: t => step.feedChunk(h1, h2, t)
+      }
     }
 
   final def zip[B](other: Step[F, E, B]): Step[F, E, (A, B)] = other match {
-    case NoLeftovers(otherValue) => new NoLeftovers((value, otherValue))
-    case WithLeftovers(otherValue, otherRemaining) => new WithLeftovers(
+    case Done(otherValue, otherRemaining) => Done(
       (value, otherValue),
       if (remaining.size <= otherRemaining.size) remaining else otherRemaining
     )
