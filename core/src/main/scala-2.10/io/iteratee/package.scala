a4ede90ea8115d3283998e37c848332e5a01f358
Travis Brown <travisrobertbrown@gmail.com>
2016-10-05 11:40:31 -0500

    Use custom NonEmptyVector for 2.10 instead of OneAnd

--- core/src/main/scala-2.10/io/iteratee/package.scala
+++ core/src/main/scala-2.10/io/iteratee/package.scala
@@ -1,21 +1,28 @@
 package io
 
-import cats.{ Semigroup, Traverse }
-import cats.data.OneAnd
-import cats.instances.VectorInstances
+import cats.{ Applicative, Eval, Foldable, Semigroup, Traverse }
 
-package object iteratee extends VectorInstances {
-  final type NonEmptyVector[A] = OneAnd[Vector, A]
-
-  implicit final class NonEmptyVectorOps[A](underlying: OneAnd[Vector, A]) {
-    def toVector: Vector[A] = underlying.unwrap
-    def length: Int = underlying.length
+package iteratee {
+  final case class NonEmptyVector[A](head: A, tail: Vector[A]) {
+    def toVector: Vector[A] = head +: tail
+    def length: Int = tail.size + 1
     def reduce(implicit S: Semigroup[A]): A = S.combineAllOption(toVector).get
+    def map[B](f: A => B): NonEmptyVector[B] = NonEmptyVector(f(head), tail.map(f))
+    def foldLeft[B](b: B)(f: (B, A) => B): B = tail.foldLeft(f(b, head))(f)
+    def toCatsNonEmptyVector: cats.data.NonEmptyVector[A] = cats.data.NonEmptyVector(head, tail)
   }
 
   final object NonEmptyVector {
-    def apply[A](h: A, t: Vector[A]): NonEmptyVector[A] = OneAnd(h, t)
-    def fromVectorUnsafe[A](vector: Vector[A]): NonEmptyVector[A] = OneAnd(vector.head, vector.tail)
-    val catsDataInstancesForNonEmptyVector: Traverse[NonEmptyVector] = OneAnd.catsDataTraverseForOneAnd[Vector]
+    private[this] val F: Traverse[Vector] = cats.instances.vector.catsStdInstancesForVector
+
+    def fromVectorUnsafe[A](vector: Vector[A]): NonEmptyVector[A] = NonEmptyVector(vector.head, vector.tail)
+
+    implicit val catsDataInstancesForNonEmptyVector: Traverse[NonEmptyVector] = new Traverse[NonEmptyVector] {
+      def foldLeft[A, B](fa: NonEmptyVector[A], b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
+      def foldRight[A, B](fa: NonEmptyVector[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+        Eval.defer(f(fa.head, F.foldRight(fa.tail, lb)(f)))
+      def traverse[G[_], A, B](fa: NonEmptyVector[A])(f: A => G[B])(implicit G: Applicative[G]): G[NonEmptyVector[B]] =
+        G.map2Eval(f(fa.head), Eval.always(F.traverse(fa.tail)(f)))(NonEmptyVector(_, _)).value
+    }
   }
 }
