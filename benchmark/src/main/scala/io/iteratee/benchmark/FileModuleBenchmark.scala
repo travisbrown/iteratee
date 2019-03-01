ad892b5d201df9ef160146f55fd5f3e0f5beb98a
Travis Brown <travisrobertbrown@gmail.com>
2019-03-01 05:22:46 -0600

    More benchmark project clean-up

--- benchmark/src/main/scala/io/iteratee/benchmark/FileModuleBenchmark.scala
+++ benchmark/src/main/scala/io/iteratee/benchmark/FileModuleBenchmark.scala
@@ -6,19 +6,11 @@ import cats.free.Free
 import cats.instances.int._
 import cats.instances.try_._
 import io.iteratee.{ Enumerator, Iteratee }
-import io.iteratee.{ monix => m }
-import io.iteratee.{ scalaz => s }
-import io.iteratee.scalaz.ScalazInstances
 import java.io.InputStream
 import java.util.concurrent.TimeUnit
-import monix.eval.{ Task => TaskM }
 import org.openjdk.jmh.annotations._
 import scala.Predef.refArrayOps
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
 import scala.util.Try
-import scalaz.concurrent.Task
 
 /**
  * Compare the performance of iteratee operations.
@@ -30,12 +22,10 @@ import scalaz.concurrent.Task
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
-class FileModuleBenchmark extends ScalazInstances {
+class FileModuleBenchmark {
   private def bartebly: InputStream = getClass.getResourceAsStream("/io/iteratee/examples/pg/11231/11231.txt")
 
   def linesIO: Enumerator[IO, String] = io.iteratee.files.modules.io.readLinesFromStream(bartebly)
-  def linesS: Enumerator[Task, String] = s.task.readLinesFromStream(bartebly)
-  def linesM: Enumerator[TaskM, String] = m.task.readLinesFromStream(bartebly)
   def linesTF: Enumerator[Free[Try, ?], String] = FreeTryModule.readLinesFromStream(bartebly)
 
   def words[F[_]: Monad](line: String): Enumerator[F, String] = Enumerator.enumVector(line.split(" ").toVector)
@@ -50,19 +40,6 @@ class FileModuleBenchmark extends ScalazInstances {
 
   @Benchmark
   def avgWordLengthIO: Double = linesIO.flatMap(words[IO]).into(avgLen).unsafeRunSync
-
-  @Benchmark
-  def avgWordLengthS: Double = linesS.flatMap(words[Task]).into(avgLen).unsafePerformSync
-
-  @Benchmark
-  def avgWordLengthM: Double = {
-    import m.task._
-
-    Await.result(
-      linesM.flatMap(words[TaskM]).into(avgLen).runAsync(monix.execution.Scheduler.Implicits.global),
-      Duration.Inf
-    )
-  }
 
   @Benchmark
   def avgWordLengthTF: Double = linesTF.flatMap(words[Free[Try, ?]]).into(avgLen[Free[Try, ?]]).runTailRec.get
