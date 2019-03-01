6c3e1a67786f3ef5e7896a533db2a37893f0819d
Travis Brown <travisrobertbrown@gmail.com>
2019-03-01 04:37:34 -0600

    Some post-cats-effect-ification clean-up

--- monix/shared/src/main/scala/io/iteratee/monix/DefaultTaskModule.scala
+++
@@ -1,8 +0,0 @@
-package io.iteratee.monix
-
-import cats.effect.Sync
-import monix.eval.Task
-
-trait DefaultTaskModule extends TaskModule with MonixInstances {
-  final protected val F: Sync[Task] = monixTaskSync
-}
